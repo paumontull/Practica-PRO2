ee389698dabe69ada0918a2a86654c2c101d5fba
paumontull <pau.montull@est.fib.upc.edu>
2016-05-02 22:18:28 +0200

    Update Frase.hh

--- Practica/Frase.hh
+++ Practica/Frase.hh
@@ -5,6 +5,8 @@
 #include <vector>
 #include <string>
 
+using namespace std;
+
 class Frase{
 
 private:
@@ -15,7 +17,7 @@ private:
 	// "?" o no (i.e. un nom d'un autor es considera una frase per tenir
 	// més d'una paraula però no acaba en caracter marca)
     
-	vector<string> Frase;
+	vector<string> frase;
     
 public:
 	//Constructores
@@ -26,22 +28,36 @@ public:
 	
 	//Destructora
 	
-	~Text();
+	~Frase();
 	
 	//Modificadores
 	
-	
+	void write_iesim(int i, string s) const;
+	/* Pre: cert */
+	/* Post: el valor de frase[i] és s */
 	
 	//Consultores
 	
+	string read_iesim(int i);
+	/* Pre: cert */
+	/* Post: retorna el valor de l'element i-èsim del p.i. */
 	
+	int n_paraules();
+	/* Pre: cert */
+	/* Post: retorna el nombre d'elements del p.i. */
 	
 	//Lectura i escriptura
 	
-	void llegir();
+	void llegir(istringstream iss);
 	/* Pre: el canal estandard d'entrada conté una sola frase no acabada 
 	   en '.', '!' o '?' */
 	/* Post: el p.i. és la frase resultant de separar l'entrada 
 	   en strings */
+	
+	void escriure();
+	/* Pre: cert */
+	/* Post: s'escriu al canal estandard de sortida els elements del
+	   p.i. separats per espais i si l'últim és un caràcter de 
+	   puntuació, aquest s'ajunta al penúltim string */
 };
 #endif
