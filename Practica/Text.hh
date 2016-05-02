30843bff0ab007b406452ffd9ca513326d391e4f
paumontull <pau.montull@est.fib.upc.edu>
2016-05-02 22:18:06 +0200

    Update Text.hh

--- Practica/Text.hh
+++ Practica/Text.hh
@@ -2,7 +2,6 @@
 #define TEXT_HH
 
 #include "Frase.hh"
-#include <vector>
 
 class Text{
 
@@ -11,7 +10,7 @@ private:
 	// Tipus de modul: dades
 	// Descripcio del tipus: representa un text (vector de frases)
     
-	vector<Frase> Text;
+	vector<Frase> text;
     
 public:
 	//Constructores
@@ -26,22 +25,35 @@ public:
 	
 	//Modificadores
 	
-	
+	void write_iesim(int i, const Frase& s) const;
+	/* Pre: cert */
+	/* Post: el valor de frase[i] és s */
 	
 	//Consultores
 	
+	Frase read_iesim(int i);
+	/* Pre: cert */
+	/* Post: retorna el valor de l'element i-èsim del p.i. */
 	
+	int n_frases();
+	/* Pre: cert */
+	/* Post: retorna el nombre d'elements del p.i. */
 	
 	//Lectura i escriptura
 	
-	void llegir();
-	/* Pre: el canal estandard d'entrada conté una o més frases. 
-	   L'últim string de l'entrada marca sempre el final d'una frase.
-	   Adicionalment, un string acabat en '.', '!' o '?' també marca
-		 el final d'una frase. */
+	void llegir(istringstream iss);
+	/* Pre: iss conté una o més frases. L'últim string de l'entrada
+	   marca sempre el final d'una frase. Adicionalment, un string 
+	   acabat en '.', '!' o '?' també marca el final d'una frase. */
 	/* Post: el p.i. conté el vector de frases resultant de separar
 	   l'entrada en vectors d'string. Si l'últim element de la frase
 	   acaba en '.', '!' o '?' se'n separa aquest caràcter marca
 	   i es guarda com a últim element de la frase. */
+	   
+	void escriure();
+	/* Pre: cert */
+	/* Post: s'escriu al canal estandard de sortida els elements del
+	   p.i. separats per espais i amb els caracters de puntuació
+	   ajuntats a l'string anterior */
 };
 #endif
